26fce748554dae100340c9bd5c1c63574d48bdb5
Andrii Shumada <eagleeyes91@gmail.com>
2017-03-09 16:01:46 +0200

    fix non-private modules check for unfixed version

--- mocha-strict-dependencies.js
+++ mocha-strict-dependencies.js
@@ -10,12 +10,16 @@ module.exports = function(whitelistedPackages = []) {
 			const version = deps[dependency];
 			if (whitelistedPackages.indexOf(dependency) !== -1) {
 				it(`${dependency} should have unfixed version`, () => {
-					expect(version).to.not.match(/#[a-z0-9\.]{6,8}/);
+					if (version.indexOf('github') !== -1) {
+						expect(version).to.not.match(/#[a-z0-9\.]{6,}/);
+						return;
+					}
+					expect(version).to.match(/[\^\*\~]/);
 				});
 			} else {
 				it(`${dependency} should have fixed version`, () => {
-					if (version.indexOf('git@github.com') !== -1) {
-						expect(version).to.match(/#[a-z0-9\.]{6,8}/);
+					if (version.indexOf('github') !== -1) {
+						expect(version).to.match(/#[a-z0-9\.]{6,}/);
 						return;
 					}
 					expect(version).to.not.contain('^');
--- test/test-dependencies.spec.js
+++ test/test-dependencies.spec.js
@@ -1,3 +1,3 @@
-describe('should pass for this package.json', () => {
+describe('should have fixed versions of this package.json, except of deep-extend and cnf', () => {
 	require('../mocha-strict-dependencies')(['deep-extend']);
 });
